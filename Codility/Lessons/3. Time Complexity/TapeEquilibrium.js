1ce39caa328d63a43cc1ccaa37daf29d48926c47
kangnayeon <seoultownroad@naver.com>
2024-09-27 02:41:15 +0900

    Codility TapeEquilibrium

---
+++ Codility/Lessons/3. Time Complexity/TapeEquilibrium.js
@@ -0,0 +1,17 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(A) {
+  let sum1 = 0;
+  let sum2 = A.reduce((sum, num) => sum + num, 0);
+  let diff = 100000;
+
+  for (let i = 0; i < A.length; i++) {
+    sum1 += A[i];
+    sum2 -= A[i];
+
+    if (Math.abs(sum1 - sum2) < diff) diff = Math.abs(sum1 - sum2);
+  }
+
+  return diff;
+}
