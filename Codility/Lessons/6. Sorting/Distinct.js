08519a7a17c33b0749d8abacce4757b1f2924143
kangnayeon <seoultownroad@naver.com>
2024-09-28 00:24:09 +0900

    Codility Distinct

---
+++ Codility/Lessons/6. Sorting/Distinct.js
@@ -0,0 +1,12 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(A) {
+  
+    let answer = 0;
+    let set = new Set(A);
+
+    answer = set.size;
+    return answer
+}
+}
